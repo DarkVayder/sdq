b14b78090ddb1e760cc0d73c0da4761c69b20a82
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2025-02-14 11:49:29 +0100

    feat: styled contact me

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,9 @@
 export const Contact = () => {
   return (
-    <section id="contact" className="min-h-screen flex items-center justify-center py-20 text-white">Contact Me</section>
-    
+    <section id="contact" className="min-h-screen flex items-center justify-center py-20 text-white">
+      <div className="">
+        <h2>Contact Me</h2>
+      </div>
+    </section>
   )
 };
